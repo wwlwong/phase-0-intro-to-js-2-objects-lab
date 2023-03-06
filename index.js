6ec27a483a77a855f91201df1799831f826b78f2
William Wong <william.wai.lim.wong@gmail.com>
2023-03-06 16:45:38 -0500

    Completed Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: "Michael",
+    streetAddress : "11 Broadway"
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const copyOfEmployee = {...obj};
+
+    copyOfEmployee[key] = value;
+
+    return copyOfEmployee;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const copyOfEmployee = {...obj};
+
+    delete copyOfEmployee[key];
+
+    return copyOfEmployee
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
